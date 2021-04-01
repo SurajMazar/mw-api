c9fc5cfa967d0ead6c8c3f3915efd7b938d4de04
surajMazar <suraj.mazar@gmail.com>
2021-04-01 11:45:34 +0545

    genre sectionns

---
+++ src/http/repositories/manga/manga.interface.ts
@@ -0,0 +1,3 @@
+export interface MangaInterface{
+  index():any;
+}
---
+++ src/http/repositories/manga/manga.repository.ts
@@ -0,0 +1,8 @@
+import { MangaInterface } from "./manga.interface";
+
+
+export class MangaRepository implements MangaInterface{
+  public index(){
+    
+  }
+}
