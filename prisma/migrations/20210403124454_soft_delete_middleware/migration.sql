41983f08ae058cd204453b1d6caf6df3bfbb5916
surajMazar <suraj.mazar@gmail.com>
2021-04-05 12:17:19 +0545

    heroku deployment

--- prisma/migrations/20210403124454_soft_delete_middleware/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `genre` ADD COLUMN     `deleted` BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE `role` ADD COLUMN     `deleted` BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE `user` ADD COLUMN     `deleted` BOOLEAN NOT NULL DEFAULT false;
